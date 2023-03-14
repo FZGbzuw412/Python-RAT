aefb530cfbfa4bba8e8ef803d11f6ac56f050dc9
finnaminope <91102755+finnaminope@users.noreply.github.com>
2023-03-14 16:02:46 -0600

    Add files via upload

---
+++ installwithargs.bat
@@ -0,0 +1,2 @@
+pip3 install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r client_requirements.txt
+pip3 install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r server_requirements.txt
