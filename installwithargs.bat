7d2b9f0d172555755e112ee0ed3bd57241fbe66d
finnaminope <91102755+finnaminope@users.noreply.github.com>
2023-03-15 14:40:41 -0600

    Revert "Update installwithargs.bat"
    This reverts commit 2fa005e0c03b0da8148aa6c24fe03370384c01a9.

--- installwithargs.bat
+++ installwithargs.bat
@@ -1,2 +1,2 @@
-python -m pip install --global http.sslVerify false -r client_requirements.txt
-python -m pip install --global http.sslVerify falseg -r server_requirements.txt
+python -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r client_requirements.txt
+python -m pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r server_requirements.txt
